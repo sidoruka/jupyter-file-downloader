4e5703ee2a6a11312b20b2567c1f5712481f9b31
Aleksandr Sidoruk <aleksandr_sidoruk@epam.com>
2022-12-01 14:44:28 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 setup(
     name="JupyterFileDownloader",
     version="1.0.0",
-    include_package_data=True
+    include_package_data=True,
+    packages=['JupyterFileDownloader']
 )
